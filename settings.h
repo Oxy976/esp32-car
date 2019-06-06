ce02b177dd29eac5e674332cdec29707f8d6de09
Oxy976 <ghost_mail2005-musor@yahoo.com>
2019-06-06 19:58:39 +0300

    Update settings.h

--- settings.h
+++ settings.h
@@ -7,13 +7,13 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 // пины к которым подключен контроллер моторов
 // мотор 1:
 int motor1Pin1 = 27;
-int motor1Pin2 = 26;
+int motor1Pin2 = 16;
 int enable1Pin = 14;
  
 // мотор 2:
-int motor2Pin1 = 33;
+int motor2Pin1 = 17;
 int motor2Pin2 = 25;
-int enable2Pin = 32;
+int enable2Pin = 26;
  
 // переменные для свойств широтно-импульсной модуляции (ШИМ):
 const int freq = 30000;
